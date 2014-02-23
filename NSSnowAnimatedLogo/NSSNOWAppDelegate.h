ea06d16bf28fe746f9062a11b5b5c56ab8eb4b2f
Matt Glover <matt@duchysoftware.com>
2014-02-23 23:39:39 +0000

    Initial Commit

---
+++ NSSnowAnimatedLogo/NSSNOWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NSSNOWAppDelegate.h
+//  NSSnowAnimatedLogo
+//
+//  Created by Matt Glover on 23/02/2014.
+//  Copyright (c) 2014 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSSNOWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSSnowAnimatedLogo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSSnowAnimatedLogo
+//
+//  Created by Matt Glover on 23/02/2014.
+//  Copyright (c) 2014 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NSSNOWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([NSSNOWAppDelegate class]));
+  }
+}
